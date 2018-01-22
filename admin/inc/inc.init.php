cf438ceb32941eb875d0e04c8b923e9b4fd8e18b
johandas <johan.dasilva@lepoles.com>
2018-01-22 10:31:54 +0100

    c32

--- admin/inc/inc.init.php
+++ admin/inc/inc.init.php
@@ -2,10 +2,10 @@
 // Ouverture de session_start()
 session_start();
 // Déclaration des variables
-$hote = 'db717621239.db.1and1.com'; // Le chemin vers le serveur
-$bdd = 'db717621239'; // Le nom de la base de donnée
-$utilisateur ="dbo717621239"; // Le nom d'utilisateur pour se connecter
-$mdp = 'Aqwzsx08$'; // Mot de passe local
+$hote = 'localhost'; // Le chemin vers le serveur
+$bdd = 'cv'; // Le nom de la base de donnée
+$utilisateur ="root"; // Le nom d'utilisateur pour se connecter
+$mdp = ''; // Mot de passe local
 $msg = '';
 // Connexion à la base de donnée
 $pdo = new PDO('mysql:host=' . $hote . ';dbname=' . $bdd, $utilisateur, $mdp, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
--- inc/connexion.php
+++ inc/connexion.php
@@ -1,11 +1,17 @@
 <?php
+
 // Déclaration des variables
-$hote = 'db717621239.db.1and1.com'; // Le chemin vers le serveur
-$bdd = 'db717621239'; // Le nom de la base de donnée
-$utilisateur ="dbo717621239"; // Le nom d'utilisateur pour se connecter
-$mdp = 'Aqwzsx08$'; // Mot de passe local
+$hote = 'localhost'; // Le chemin vers le serveur
+$bdd = 'cv'; // Le nom de la base de donnée
+$utilisateur ="root"; // Le nom d'utilisateur pour se connecter
+$mdp = ''; // Mot de passe local
 $msg = '';
 // Connexion à la base de donnée
 $pdo = new PDO('mysql:host=' . $hote . ';dbname=' . $bdd, $utilisateur, $mdp, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-define('RACINE_FRONT', 'johan-da-silva.fr/admin/');
+// Déclaration des chemins
+define('RACINE_CV', 'www.johan-da-silva.fr/admin/');
+define('URL', 'http://www.johan-da-silva.fr/');
+
+// require de mon fichier contenant mes fonctions.
+require('inc.fonctions.php');
